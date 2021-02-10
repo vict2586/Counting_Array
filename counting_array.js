5601c4b9c871164d1e3e5a26063d8813528e8095
Victoria <vigi2586@stud.kea.dk>
2021-02-10 15:41:59 +0100

    js done

--- counting_array.js
+++ counting_array.js
@@ -2,6 +2,26 @@
 
 document.addEventListener("DOMContentLoaded", start);
   
+const array = [];
+let numbers = 0;
 
 function start() {
+
+  //add item in the beginning with unshift
+  array.unshift(numbers);
+
+  //Add +1s
+  numbers++;
+
+  //can not be longer than 9 items
+  //get item from array if number is bigger than 9
+  if (numbers > 9) {
+    array.pop(array);
+  }
+
+  setTimeout(start, 1000);
+
+  //Write in the console
+  console.log(array);
+
 };

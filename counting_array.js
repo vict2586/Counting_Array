d88957e453d4a2fce2fb74c5cf6309d3d8e19232
Victoria <vigi2586@stud.kea.dk>
2021-02-10 15:43:52 +0100

    js done

--- counting_array.js
+++ counting_array.js
@@ -10,18 +10,15 @@ function start() {
   //add item in the beginning with unshift
   array.unshift(numbers);
 
-  //Add +1s
   numbers++;
 
-  //can not be longer than 9 items
-  //get item from array if number is bigger than 9
+  //can not be longer than 9
   if (numbers > 9) {
     array.pop(array);
   }
 
   setTimeout(start, 1000);
 
-  //Write in the console
   console.log(array);
 
 };
